ff44aea825a22cd2025f5e3f80a7b814e631d2c5
Adam <github@asanderson.org>
2013-07-18 17:18:00 -0700

    Removed engine/railties tie-in for now

--- lib/bootstrap/rails/three.rb
+++ lib/bootstrap/rails/three.rb
@@ -4,11 +4,11 @@ require "bootstrap/rails/three/version"
 module Bootstrap
   module Rails
     module Three
-      if ::Rails.version < '3.1'
-      	require 'bootstrap-rails-three/railtie'
-      else
-      	require 'bootstrap-rails-three/engine'
-      end
+      # if ::Rails.version < '3.1'
+      # 	require 'bootstrap-rails-three/railtie'
+      # else
+      # 	require 'bootstrap-rails-three/engine'
+      # end
     end
   end
 end
